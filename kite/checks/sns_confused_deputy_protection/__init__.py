91b53f7140056acc8308af73a030ec111bcf331a
Andy Caine <andy@hyperscale.consulting>
2025-05-21 08:30:46 +0100

    Deleted old packages

--- kite/checks/sns_confused_deputy_protection/__init__.py
+++ kite/checks/sns_confused_deputy_protection/__init__.py
@@ -1,5 +1,13 @@
 """SNS confused deputy protection check package."""
 
-from kite.checks.sns_confused_deputy_prevention.check import check_sns_confused_deputy_prevention
+from kite.checks.sns_confused_deputy_protection.check import (
+    check_sns_confused_deputy_protection,
+    CHECK_ID,
+    CHECK_NAME,
+)
 
-__all__ = ["check_sns_confused_deputy_prevention"]
+__all__ = [
+    "check_sns_confused_deputy_protection",
+    "CHECK_ID",
+    "CHECK_NAME",
+]

a03cb656fd9d191e227c5e6afab9603890afb477
Andy Caine <andy@hyperscale.consulting>
2025-06-24 09:38:06 +0100

    Handle SecurityHub not being configured a bit better

--- kite/securityhub.py
+++ kite/securityhub.py
@@ -11,12 +11,18 @@ def get_action_targets(session: boto3.Session, region: str) -> List[Dict[str, An
         session: The session to use for the API call.
         region: The region to get the action targets for.
     """
-    client = session.client("securityhub", region_name=region)
-    paginator = client.get_paginator("describe_action_targets")
-    action_targets = []
-    for page in paginator.paginate():
-        action_targets.extend(page["ActionTargets"])
-    return action_targets
+    try:
+        client = session.client("securityhub", region_name=region)
+        paginator = client.get_paginator("describe_action_targets")
+        action_targets = []
+        for page in paginator.paginate():
+            action_targets.extend(page["ActionTargets"])
+        return action_targets
+    except (
+        client.exceptions.InvalidAccessException,
+        client.exceptions.ResourceNotFoundException,
+    ):
+        return []
 
 
 def get_automation_rules(session: boto3.Session, region: str) -> List[Dict[str, Any]]:
@@ -27,6 +33,12 @@ def get_automation_rules(session: boto3.Session, region: str) -> List[Dict[str, 
         session: The session to use for the API call.
         region: The region to get the automation rules for.
     """
-    client = session.client("securityhub", region_name=region)
-    response = client.list_automation_rules()
-    return response["AutomationRulesMetadata"]
+    try:
+        client = session.client("securityhub", region_name=region)
+        response = client.list_automation_rules()
+        return response["AutomationRulesMetadata"]
+    except (
+        client.exceptions.InvalidAccessException,
+        client.exceptions.ResourceNotFoundException,
+    ):
+        return []

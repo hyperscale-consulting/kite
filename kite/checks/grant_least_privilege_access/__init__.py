620952020406c598ad114f658cb76cdbecf87a29
Andy Caine <andy@hyperscale.consulting>
2025-07-15 15:18:05 +0100

    chore: deleted old check

--- kite/checks/grant_least_privilege_access/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Grant least privilege access check module."""
-
-from kite.checks.grant_least_privilege_access.check import (
-    check_grant_least_privilege_access,
-)
-from kite.checks.grant_least_privilege_access.check import CHECK_ID
-from kite.checks.grant_least_privilege_access.check import CHECK_NAME
-
-__all__ = ["check_grant_least_privilege_access", "CHECK_ID", "CHECK_NAME"]

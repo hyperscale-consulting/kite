858e86041de62d1fc081e5faa71ade706545ce0b
Andy Caine <andy@hyperscale.consulting>
2025-07-29 16:20:20 +0100

    chore: fix pyright issues, make private functions private

--- kite/ecs.py
+++ kite/ecs.py
@@ -1,5 +1,3 @@
-"""ECS service module for Kite."""
-
 from typing import Any
 
 import boto3
@@ -21,39 +19,28 @@ def get_clusters(session: boto3.Session, region: str) -> list[str]:
     paginator = ecs_client.get_paginator("list_clusters")
     for page in paginator.paginate():
         for cluster_arn in page.get("clusterArns", []):
-            cluster = get_cluster(ecs_client, cluster_arn)
-            cluster["services"] = get_services(ecs_client, cluster_arn)
+            cluster = _get_cluster(ecs_client, cluster_arn)
+            cluster["services"] = _get_services(ecs_client, cluster_arn)
             clusters.append(cluster)
 
     return clusters
 
 
-def get_cluster(client: boto3.client, name: str) -> dict[str, Any]:
-    """
-    Get an ECS cluster by name.
-    """
+def _get_cluster(client, name) -> dict[str, Any]:
     return client.describe_clusters(clusters=[name])["clusters"][0]
 
 
-def get_services(client: boto3.client, cluster_arn: str) -> list[str]:
-    """
-    Get all services in an ECS cluster.
-    """
+def _get_services(client, cluster_arn: str) -> list[str]:
     paginator = client.get_paginator("list_services")
     services = []
     for page in paginator.paginate(cluster=cluster_arn):
         for service_arn in page.get("serviceArns", []):
-            service = get_service(client, service_arn, cluster_arn)
+            service = _get_service(client, service_arn, cluster_arn)
             services.append(service)
     return services
 
 
-def get_service(
-    client: boto3.client, service_arn: str, cluster_arn: str
-) -> dict[str, Any]:
-    """
-    Get an ECS service by ARN.
-    """
+def _get_service(client, service_arn: str, cluster_arn: str) -> dict[str, Any]:
     return client.describe_services(cluster=cluster_arn, services=[service_arn])[
         "services"
     ][0]

3865ad453d9ef84379b217e974f7fb9c51646aac
Andy Caine <andy@hyperscale.consulting>
2025-07-29 10:09:47 +0100

    feat: handle SubscriptionRequiredException from detective client (#7)

--- kite/detective.py
+++ kite/detective.py
@@ -1,16 +1,22 @@
 import boto3
+from botocore.exceptions import ClientError
 
 
 def get_graphs(session: boto3.Session, region: str) -> list[dict[str, object]]:
     client = session.client("detective", region_name=region)
-    response = client.list_graphs()
-    graphs = []
-    for graph in response["GraphList"]:
-        arn = graph["Arn"]
-        members = get_members(client, arn)
-        graph["Members"] = members
-        graphs.append(graph)
-    return graphs
+    try:
+        response = client.list_graphs()
+        graphs = []
+        for graph in response["GraphList"]:
+            arn = graph["Arn"]
+            members = get_members(client, arn)
+            graph["Members"] = members
+            graphs.append(graph)
+        return graphs
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "SubscriptionRequiredException":
+            return []
+        raise e
 
 
 def get_members(client, arn: str) -> list[dict[str, object]]:

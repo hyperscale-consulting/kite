eefe17bf9fb97fd9cd00f9f53eabf81b75e5c554
Andy Caine <andy@hyperscale.consulting>
2025-07-29 12:07:30 +0100

    feat: handle SubscriptionRequiredException from networkfirewall client (#7)

--- kite/networkfirewall.py
+++ kite/networkfirewall.py
@@ -1,13 +1,20 @@
 import boto3
+from botocore.exceptions import ClientError
 
 
 def get_firewalls(session: boto3.Session, region: str) -> list[dict[str, object]]:
-    client = session.client("network-firewall", region_name=region)
-    paginator = client.get_paginator("list_firewalls")
-    firewalls = []
-    for page in paginator.paginate():
-        for firewall in page["Firewalls"]:
-            detail = client.describe_firewall(FirewallArn=firewall["FirewallArn"])
-            firewall["Detail"] = detail
-            firewalls.append(firewall)
-    return firewalls
+    try:
+        client = session.client("network-firewall", region_name=region)
+        paginator = client.get_paginator("list_firewalls")
+        firewalls = []
+        for page in paginator.paginate():
+            print(page)
+            for firewall in page["Firewalls"]:
+                detail = client.describe_firewall(FirewallArn=firewall["FirewallArn"])
+                firewall["Detail"] = detail
+                firewalls.append(firewall)
+        return firewalls
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "SubscriptionRequiredException":
+            return []
+        raise e

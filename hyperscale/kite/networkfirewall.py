6afe7f4b7dba7b216f4e9b4678b6e41a58b6895f
Andy Caine <andy@hyperscale.consulting>
2025-07-30 11:26:10 +0100

    fix: remove print

--- hyperscale/kite/networkfirewall.py
+++ hyperscale/kite/networkfirewall.py
@@ -8,7 +8,6 @@ def get_firewalls(session: boto3.Session, region: str) -> list[dict[str, object]
         paginator = client.get_paginator("list_firewalls")
         firewalls = []
         for page in paginator.paginate():
-            print(page)
             for firewall in page["Firewalls"]:
                 detail = client.describe_firewall(FirewallArn=firewall["FirewallArn"])
                 firewall["Detail"] = detail
